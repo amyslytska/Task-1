616be730406a76b1b6984210388b47d6e9131f03
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-11-24 09:53:31 +0200

    Update summing.js

--- summing.js
+++ summing.js
@@ -1,8 +1,9 @@
-function summing(list){
-  for (let i = 0; i < list.length; i++) {
-       total += list[i]
-    }
-    return total
-}
-const sallary = [12000, 13000, 20000]
-console.log(summing(sallary))
+let total = 0;
+function summing(list){;
+  for (let i = 0; i < list.length; i++) {;
+       total += list[i];
+    };
+    return total;
+};
+const sallary = [12000, 13000, 20000];
+console.log(summing(sallary));)
