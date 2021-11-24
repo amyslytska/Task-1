b18787ba38c09c16558448cebf2f30a52440050f
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-11-24 09:55:44 +0200

    Delete summing.js

--- summing.js
+++
@@ -1,9 +0,0 @@
-let total = 0;
-function summing(list){;
-  for (let i = 0; i < list.length; i++) {;
-       total += list[i];
-    };
-    return total;
-};
-const sallary = [12000, 13000, 20000];
-console.log(summing(sallary));
